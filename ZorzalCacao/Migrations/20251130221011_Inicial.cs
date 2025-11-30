5b3ba2a920d90e312e883cac30574d9de72649dd
jorge <jorge@LAPTOP-D8GDLQRL>
2025-11-30 19:42:15 -0400

    Fix: borrando migracion y creando otra nueva

--- ZorzalCacao/Migrations/20251130221011_Inicial.cs
+++ ZorzalCacao/Migrations/20251130221011_Inicial.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace ZorzalCacao.Migrations
 {
     /// <inheritdoc />
-    public partial class CreateIdentitySchema : Migration
+    public partial class Inicial : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
