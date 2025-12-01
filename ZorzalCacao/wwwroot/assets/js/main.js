3a5d5a364f50677c61e0a3b72fda7d3f1c2f5f68
jorge <jorge@LAPTOP-D8GDLQRL>
2025-11-30 23:47:56 -0400

    Fix: Se descomento lo relacionado con el preload

--- ZorzalCacao/wwwroot/assets/js/main.js
+++ ZorzalCacao/wwwroot/assets/js/main.js
@@ -46,13 +46,13 @@
 
   /**
    * Preloader
-   */
-  //const preloader = document.querySelector('#preloader');
-  //if (preloader) {
-  //  window.addEventListener('load', () => {
-  //    preloader.remove();
-  //  });
-  //}
+  */
+  const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove();
+    });
+  }
 
   /**
    * Scroll top button
