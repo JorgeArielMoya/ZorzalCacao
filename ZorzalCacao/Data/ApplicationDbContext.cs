56f02b3323fe23adbae064048f7607178643abed
jorge <jorge@LAPTOP-D8GDLQRL>
2025-12-01 09:38:02 -0400

    Feat: OnModel trabajado para recogida y productor

--- ZorzalCacao/Data/ApplicationDbContext.cs
+++ ZorzalCacao/Data/ApplicationDbContext.cs
@@ -15,4 +15,16 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     public DbSet<Remociones> Remociones { get; set; }
     public DbSet<FermentacionesDetalles> FermentacionesDetalles { get; set; }
     public DbSet<Sacos> Sacos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        // Relación entre Recogida y ApplicationUser (Productor)
+        builder.Entity<Recogidas>()
+            .HasOne(r => r.Productor)
+            .WithMany() // o WithMany(u => u.Recogidas) si agregas navegación inversa
+            .HasForeignKey(r => r.ProductorId)
+            .OnDelete(DeleteBehavior.Restrict); // No eliminar recogidas si se borra el usuario
+    }
 }
