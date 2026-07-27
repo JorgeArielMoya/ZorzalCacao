6d17a0e982aa8752deffca52183e40e1c9d767df
jorge <jorge@LAPTOP-D8GDLQRL>
2026-07-27 19:54:00 -0400

    Refactor: Coloca las llaves en la misma linea

--- ZorzalCacao/Data/ApplicationUser.cs
+++ ZorzalCacao/Data/ApplicationUser.cs
@@ -2,7 +2,4 @@ using Microsoft.AspNetCore.Identity;
 
 namespace ZorzalCacao.Data;
 
-// Add profile data for application users by adding properties to the ApplicationUser class
-public class ApplicationUser : IdentityUser
-{
-}
+public class ApplicationUser : IdentityUser { }
